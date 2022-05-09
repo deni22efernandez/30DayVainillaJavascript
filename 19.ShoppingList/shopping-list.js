d4e3144ec956ea62a6f17e26450abd75591e8213
Deni22e <griseldadenisfernandez@gmail.com>
2022-05-09 20:18:05 -0300

    shopping list

---
+++ 19.ShoppingList/shopping-list.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('.shopping');
+const input = document.querySelector('#item');
+const uls = document.querySelector('.list');
+let items = [];
+function handleSubmit(e){
+    e.preventDefault(); 
+    if(!e.currentTarget.item.value)
+    return;   
+    let currentItem = {
+        name : e.currentTarget.item.value,
+        id : Date.now(),
+        completed : false,
+    }
+    items.push(currentItem);   
+    uls.dispatchEvent(new CustomEvent('itemsUpdated'));
+    e.target.reset();    
+}
+
+function displayItems(){
+    let html = items.map(x=>`<li class='shopping-item'>
+    <input type='checkbox' name='check'>
+    <span>${x.name}${x.id}</span>
+    <button aria-label='remove ${x.name}' value=${x.id}>&times;</button>
+    </li>`).join('');
+    uls.innerHTML = html;
+}
+
+function mirrorToLocalStorage(){
+   localStorage.setItem('items', JSON.stringify(items));
+}
+
+function restoreFromLocalStorage(){
+    console.log('restoring from localStorage');
+    let localItems = JSON.parse(localStorage.getItem('items'));
+    if(localItems){
+        items=localItems;
+        uls.dispatchEvent(new CustomEvent('itemsUpdated'));
+    }
+}
+function deleteButton(id){
+    items = items.filter(x=>x.id!=id);
+    uls.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+form.addEventListener('submit', handleSubmit);
+uls.addEventListener('itemsUpdated', displayItems);
+uls.addEventListener('itemsUpdated', mirrorToLocalStorage);
+uls.addEventListener('click', (e)=>{
+    if(e.target.matches('button'))
+    deleteButton(parseInt(e.target.value));
+});
+restoreFromLocalStorage();
