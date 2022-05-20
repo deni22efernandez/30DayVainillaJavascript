01f4fffbd68ee301e10ce99031b30ae7bd47304d
Deni22e <griseldadenisfernandez@gmail.com>
2022-05-20 18:02:48 -0300

    AsyncTyperUI recursive

--- 23.AsyncTyperUI/scripts.js
+++ 23.AsyncTyperUI/scripts.js
@@ -4,18 +4,34 @@ function wait(ms=0){
 function getRndmNumber(min=0, max=150, randomNumber=Math.random()){
     return Math.floor(randomNumber*(max-min)+min);
 }
+// async function draw(e){
+//     let text = e.textContent;
+//     let soFar='';
+//     const {min,max}=e.dataset;
+//     const w = getRndmNumber(min,max);
+
+//     for (const letter of text) {        
+//         soFar+=letter;
+//         e.textContent=soFar;
+//         await wait(w);
+//     }
+// }
 async function draw(e){
     let text = e.textContent;
-    let soFar='';
+    let index = 1;
     const {min,max}=e.dataset;
-    const w = getRndmNumber(min,max);
-
-    for (const letter of text) {
-        
-        soFar+=letter;
-        e.textContent=soFar;
-        await wait(w);
+    
+    async function drawLetter(){
+        e.textContent=text.slice(0,index);
+        if(index<=text.length){
+            index+=1;
+            const w = getRndmNumber(min,max);
+            await wait(w);
+            drawLetter();
+        }
     }
+    drawLetter();
 }
+
 let elem = document.querySelectorAll('[data-type]');
 elem.forEach(e=>draw(e));
